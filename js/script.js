d267fe37cde86d7e5445e891702a246604259714
AntonioStr <antonio.straziota10@gmail.com>
2022-02-21 15:35:48 +0100

    Aggiunta testo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let age = prompt('Eta passeggero');
-let km = prompt('Kilometri da percorrere');
+let age = prompt('Inserire età passeggero:');
+let km = prompt('Inserire i kilometri (km) da percorrere:');
 const prezzo = 0.21;
 const disc20 = 20;
 const disc40 = 40;
@@ -8,10 +8,10 @@ let biglietto = (km * prezzo);
 
 if (age < 18){
     let prezzoScontato20 = ((biglietto / 100) * disc20);
-    document.write(prezzoScontato20.toFixed(1));
+    document.write('Il prezzo del tuo biglietto è: ' + prezzoScontato20.toFixed(2) + '€');
 } else if (age > 65) {
     let prezzoScontato40 = ((biglietto / 100) * disc40);
-    document.write(prezzoScontato40.toFixed(1));
+    document.write('Il prezzo del tuo biglietto è: ' + prezzoScontato40.toFixed(2) + '€');
 } else {
     document.write(biglietto.toFixed(1));
 }
